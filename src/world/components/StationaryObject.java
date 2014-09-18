60f2064ea13f2d8b1a913a40e1ee42681ac62a0d
Kalo Pilato <mista.kay@gmail.com>
2014-09-18 21:20:41 +1200

    Added StationaryObject interface

---
+++ src/world/components/StationaryObject.java
@@ -0,0 +1,10 @@
+package world.components;
+
+/**
+ * The interface for stationary objects in this game world
+ * @author Kalo Pilato - ID: 300313803
+ *
+ */
+public interface StationaryObject {
+
+}
