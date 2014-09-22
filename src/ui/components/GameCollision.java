dbe9522720dd0faa58bb6740e5733267e43fac9a
Vivian Stewart <vivichrist@gmail.com>
2014-09-23 11:06:08 +1200

    new parition class for walls

--- src/ui/components/GameCollision.java
+++ src/ui/components/GameCollision.java
@@ -114,7 +114,7 @@ public class GameCollision
 				switch( map[i][j] )
 				{
 				case WALL :
-					toDraw.add( new Partition( center, north, east, south, west, new Point( i, j ), scale ) );
+					toDraw.add( new Partition( north, east, south, west, new Point( i, j ), scale ) );
 					break;
 				case DOOR :
 					// TODO: very very bung!!
