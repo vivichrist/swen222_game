deab7633f86f0c4cc6b7b1eae803d6f5a6e4e0ac
zhaojiang chang <zhaojiangchangnz@gmail.com>
2014-10-01 02:00:39 +1300

    create a new Timer class for server count-down

---
+++ src/ServerClients/Timer.java
@@ -0,0 +1,73 @@
+package ServerClients;
+/**
+ * @author  Zhaojiang Chang
+ * 
+ * four mins count down 
+ * this class will call from server class, once clients size >=3 will start timing.
+ * */
+public class Timer {
+	
+	int countDownGame = 240;//4mins in seconds
+	int countDownPre = 10;//wait 10senconds before start game
+		public Timer(){
+		}
+		/**
+		 * timerGameStart method is going to count down 4 mins for each client
+		 * */
+		
+		public void timerGameStart(){
+			long currentTime= System.currentTimeMillis();//current system 
+			long i=System.currentTimeMillis();
+			long temp=currentTime;//assign currentTime to temp 
+			int gameCycle = 240000;//4mins in milliseconds
+
+		while (i-currentTime<gameCycle)
+		{     
+
+			if ((i-temp)== 1000 )
+			{
+				System.out.println(" you got "+countDownGame+" second to collect all the items");
+
+				countDownGame = (int) (gameCycle/1000-(i-currentTime)/1000);
+				temp+=1000;
+			}
+			i=System.currentTimeMillis();
+		}
+
+		//System.out.println("Stopped!!");
+
+	}
+		/**
+		 * timerWaiting it will wait for 10 second then start the game
+		 * 
+		 * */
+		public void timerWaiting(){
+			long currentTime= System.currentTimeMillis();//current system 
+			long i=System.currentTimeMillis();
+			long temp=currentTime;//assign currentTime to temp 
+			int gameCycle = 10000;//10seconds in milliseconds
+			
+			while (i-currentTime-2000<gameCycle)
+			{     
+
+				if ((i-temp)== 1000 )
+				{
+					System.out.println(" Game will start in "+countDownPre+" second");
+
+					countDownPre = (int) (gameCycle/1000-(i-currentTime)/1000);
+					temp+=1000;
+				}
+				i=System.currentTimeMillis();
+			}
+
+			//System.out.println("Stopped!!");
+
+		}
+		public int getCountDownGame() {
+			return countDownGame;
+		}
+		public int getCountDownPre() {
+			return countDownPre;
+		}
+		
+}
