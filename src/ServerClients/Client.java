d5a6202456ffb8c41848cd184e099641fe96cb8a
zhaojiang chang <zhaojiangchangnz@gmail.com>
2014-10-02 03:46:08 +1300

    upaded client and server deleted clientThread, serverThread

--- src/ServerClients/Client.java
+++ src/ServerClients/Client.java
@@ -4,87 +4,68 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 import javax.swing.JOptionPane;
+
+import world.game.GameBuilder;
 /**
  * @author  Zhaojiang Chang
  * 
  * */
-public class Client {
+public class Client extends Thread {
+	private InetAddress ipAddress;
+	private DatagramSocket socket;
 	private static final int SERVER_PORT = 3000;
-	private Socket socket;
+	private GameBuilder game;
 	private PrintStream ps;
 	private BufferedReader brServer;
 	private BufferedReader brClient;
-	
-	public void init(){
-		try{
-			//initialize input from keyboard
-			brClient = new BufferedReader(new InputStreamReader(System.in));//get input from game 
-			//connect to server
-			socket = new Socket("LocalHost", SERVER_PORT);
-			//obtain input and output from socket
-			ps = new PrintStream(socket.getOutputStream());
-			brServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String tip="";
-			//request input input player name
-			while(true){
-				String userName = JOptionPane.showInputDialog(tip+ " Input player name: ");
-				ps.println(ServerClientProtocal.playerName + userName +ServerClientProtocal.playerName);
-				String response = brServer.readLine();
-				//if name already taken by other player, player require to choice another name
-				if(response.equals(ServerClientProtocal.playerNameRep)){
-					tip = "name already been used, please choose another name! ";
-					continue;
-				}
-				//if server response success connected, break out loop
-				if(response.equals(ServerClientProtocal.loginSuccess)){
-					break;
-				}
+	//public Client(GameBuilder game, String ipAddress){
+	public Client(String ipAddress){
+		//this.game = game;
+		try {
+			this.socket = new DatagramSocket();
+			this.ipAddress = InetAddress.getByName(ipAddress);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	public void run(){
+		
+		while(true){
+			byte[]data = new byte[1024];
+			DatagramPacket packet = new DatagramPacket(data, data.length);
+			try{
+				socket.receive(packet);
+			}catch(IOException e){
+				e.printStackTrace();
 			}
-			
-		}catch(UnknownHostException ex){
-			System.out.println("could not find server, start server before client");
-		}catch(IOException e){
-			System.out.println("unable to connect to server, redo log in");
-			closeAll();
-			System.exit(1);
-			
-		}
-		new ClientThread(brServer,ps).start();
+			System.out.println("Server >" +new String(packet.getData()));
+			}
+	
 		
 	}
-	private void readAndSend(){
-		try{
-			String line = null;
-		
-		while((line = brClient.readLine())!=null){
-		  ps.println(line);
-			
+	private void sendData(byte[]data){
+		DatagramPacket packet = new DatagramPacket(data, data.length, ipAddress, SERVER_PORT);
+		try {
+			socket.send(packet);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	
 	}
-		catch(IOException ex){
-			System.out.println("can not connect, redo the log in");
-			closeAll();
-			System.exit(1);
-		}
-	}
-	private void closeAll() {
-		// TODO Auto-generated method stub
-		try{
-    		if(brServer!=null) brServer.close();
-    		if(brClient!=null) brClient.close();
-    		if(ps!=null) ps.close();
-    		if(socket!=null) socket.close();
-    	}catch(IOException ex){
-    		ex.printStackTrace();
-    	}
-	}
-	public static void main(String[]args){
-		Client client = new Client();
-		client.init();
-		client.readAndSend();
+	public static void main(String[]arg0){
+		Client c = new Client("127.0.0.1");
+		c.start();
 	}
 }
+		
--- src/ServerClients/Server.java
+++ src/ServerClients/Server.java
@@ -3,39 +3,69 @@
  */
 package ServerClients;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.HashMap;
+
+import world.game.GameBuilder;
 
 /**
  * @author  Zhaojiang Chang
  *
  */
-public class Server {
+public class Server extends Thread {
 	private static final int SERVER_PORT = 3000;
-	public static clientNameMap<String, PrintStream> clients = new clientNameMap<String, PrintStream>();
-	public HashMap<String, String>my = new HashMap<String,String>();
-	public void init(){
+	private DatagramSocket socket;
+	private GameBuilder game;
 
-		try{
-			//create a new serverScoket
-			ServerSocket ss = new ServerSocket(SERVER_PORT);
-			
-			
-			while(true){
-				Socket socket = ss.accept();
-				new ServerThread(socket).start();
-				
-			}
-
-		}catch(IOException e){
-			System.out.println("Server start fail, is it port "+ SERVER_PORT+" been used by other server?");
-		}
+	//public Server(GameBuilder game){
+	//	this.game = game;
+	public Server(){
+		try {
+			this.socket = new DatagramSocket();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} 
 	}
-	public static void main(String[] args){
-		Server server = new Server();
-		server.init();
+	public void run(){
+
+		while(true){
+			byte[]data = new byte[1024];
+			DatagramPacket packet = new DatagramPacket(data, data.length);
+			try{
+				socket.receive(packet);
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+			String message = new String(packet.getData());
+			if(message.equalsIgnoreCase("ping")){
+				System.out.println("CLIENT > "+ new String(packet.getData()));
+				sendData("Pong".getBytes(), packet.getAddress(), packet.getPort());
+			}
+		}
+
+
+	}
+	private void sendData(byte[]data, InetAddress ipAddress, int port){
+		DatagramPacket packet = new DatagramPacket(data, data.length, ipAddress, SERVER_PORT);
+		try {
+			socket.send(packet);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	public static void main(String[]arg0){
+		Server c = new Server();
 	}
 }
+
