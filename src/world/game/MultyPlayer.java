29840b101f2d0813f130ca896e7d32fac4f10bba
zhaojiang chang <zhaojiangchangnz@gmail.com>
2014-10-04 22:00:51 +1300

    add one more parameter Map - may change latter(update the current map)

---
+++ src/world/game/MultyPlayer.java
@@ -0,0 +1,31 @@
+package world.game;
+
+import java.awt.Point;
+import java.net.InetAddress;
+
+import world.components.Map;
+import world.components.TokenType;
+
+
+public class MultyPlayer extends Player {
+
+	public InetAddress ipAddress;
+	public int port;
+	int x;
+	int y;
+	private Point position;
+	private Map floor;
+
+	public MultyPlayer( String username,Point position, TokenType type,InetAddress ipAddress, int port, Map fl) {
+		super(username, type);
+		this.ipAddress = ipAddress;
+		this.port = port;
+		this.position = position;
+		this.floor = fl;
+	}
+
+	public Point getPosition() {
+		return position;
+	}
+
+}
