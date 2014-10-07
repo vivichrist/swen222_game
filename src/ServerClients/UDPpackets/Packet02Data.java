67acefb0de2eb38fdacf67ef5ee73da5f514e828
zhaojiang chang <zhaojiangchangnz@gmail.com>
2014-10-08 01:10:46 +1300

    updated packet02Data add getState method return gameState given bytes

--- src/ServerClients/UDPpackets/Packet02Data.java
+++ src/ServerClients/UDPpackets/Packet02Data.java
@@ -1,6 +1,9 @@
 package ServerClients.UDPpackets;
 
 import java.awt.Point;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 import world.components.Map;
 import world.game.GameState;
@@ -10,76 +13,94 @@ import ServerClients.Server;
 
 public class Packet02Data extends UDPPakcet {
 
-    private String username;
-    private GameState state;
-    private Point point;
-    private int x;
-    private int y;
+	private String username;
+	private GameState state;
+	private Point point;
+	private int x;
+	private int y;
+	private byte[] data;
 
 
-    public Packet02Data( String username, int x, int y) {
-        super(02);
-    	System.out.println("packet02Data con 1: ");
-
-        this.username = username;
-        this.x = x;
-        this.y = y;
-        this.point = new Point(x,y);
-
-    }
-
-
-
-	public Packet02Data(GameState state) {
+	public Packet02Data( String username, int x, int y) {
 		super(02);
-		// TODO Auto-generated constructor stub
-		this.state = state;
-		//state.deserialize(data);
+		System.out.println("packet02Data con 1: ");
+
+		this.username = username;
+		this.x = x;
+		this.y = y;
+		this.point = new Point(x,y);
+
 	}
-	public Packet02Data(byte[] data) {
+
+	public Packet02Data(GameState state,byte[] data) {
 		super(02);
-    	    System.out.println("packet02Data con 2: ");
+		this.state = state;
+		this.data = data;
+		state = getState(data);
+		System.out.println("packet02Data con 2: ");
 		// TODO Auto-generated constructor stub
 		//state.deserialize(data);
-		String[] dataArray = readData(data).split(",");
-        this.username = dataArray[0];
-        this.x = Integer.parseInt(dataArray[1]);
-        this.y = Integer.parseInt(dataArray[2]);
-        point = new Point(x,y);
-       
-		
-		
+//		String[] dataArray = readData(data).split(",");
+//		this.username = dataArray[0];
+//		this.x = Integer.parseInt(dataArray[1]);
+//		this.y = Integer.parseInt(dataArray[2]);
+//		point = new Point(x,y);
+
+
+
+	}
+
+	private GameState getState(byte[] data) {
+		// TODO Auto-generated method stub
+		byte[]newData =new byte[data.length-2];
+	
+		for(int i = 2; i<data.length;i++){
+			newData[i-2] = data[i];
+		}
+		GameState obj = null;
+		  try {
+		    ByteArrayInputStream bis = new ByteArrayInputStream (newData);
+		    ObjectInputStream ois = new ObjectInputStream (bis);
+		    obj = (GameState) ois.readObject();
+		  }
+		  catch (IOException ex) {
+		    //TODO: Handle the exception
+		  }
+		  catch (ClassNotFoundException ex) {
+		    //TODO: Handle the exception
+		  }
+		  return obj;
 	}
 
 	@Override
-    public void writeData(Client client) {
-        client.sendData(getData());
-        System.out.println("packet02Data con 5: ");
-    }
+	public void writeData(Client client) {
+		client.sendData(getData());
+		System.out.println("packet02Data con 5: ");
+	}
 
-    @Override
-    public void writeData(Server server) {
-    	System.out.println("packet02Data con 3: ");
-        server.sendDataToAllClients(getData());
-        System.out.println("packet02Data con 4: ");
-    }
+	@Override
+	public void writeData(Server server) {
+		System.out.println("packet02Data con 3: ");
+		server.sendDataToAllClients(getData());
+		System.out.println("packet02Data con 4: ");
+	}
 
-    @Override
-    public byte[] getData() {
-//    	byte[]temp = state.serialize();
-//    	byte[]newData =new byte[2+temp.length];
-//    	newData[0] = '0';
-//    	newData[1] = '2';
-//    	int count = 2;
-//
-//    	for(byte b:temp){
-//    		newData[count] = b;
-//    		count++;
-//    	}
-//        return newData;
-    	return ("02" + this.username + "," + this.x + "," + this.y).getBytes();
+	@Override
+	public byte[] getData() {
+		
+		byte[]newData =new byte[2+data.length];
+		newData[0] = '0';
+		newData[1] = '2';
+		int count = 2;
 
-    }
+		for(byte d:data){
+			newData[count] = d;
+			count++;
+		}
+		return newData;
+		// 	return ("02" + this.username + "," + this.x + "," + this.y).getBytes();
+
+	}
 
 	public String getUsername() {
 		return username;
