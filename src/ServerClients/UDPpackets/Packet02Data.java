608a812b0173e5ff4e39241eb5d7535bddc608ff
zhaojiangchang <zhaojiangchangnz@gmail.com>
2014-10-07 15:41:36 +1300

    update the packet classes

--- src/ServerClients/UDPpackets/Packet02Data.java
+++ src/ServerClients/UDPpackets/Packet02Data.java
@@ -14,22 +14,30 @@ public class Packet02Data extends UDPPakcet {
     private GameState state;
     private Map floor;
     private Point point;
+    private byte[] data;
 
 
-    public Packet02Data(String username, int x, int y,Map floor) {
-        super(02);
-        this.username = username;
-        this.floor = floor;
-        this.point = new Point(x,y);
-       
-    }
+//    public Packet02Data( String username, int x, int y,Map floor) {
+//        super(02);
+//        this.username = username;
+//        this.floor = floor;
+//        this.point = new Point(x,y);
+//
+//    }
 
-    
 
+
+	public Packet02Data(GameState state) {
+		super(02);
+		// TODO Auto-generated constructor stub
+		this.state = state;
+		//state.deserialize(data);
+	}
 	public Packet02Data(byte[] data) {
 		super(02);
 		// TODO Auto-generated constructor stub
-		state.deserialize(data);
+		//state.deserialize(data);
+		this.data = data;
 	}
 
 	@Override
@@ -44,7 +52,17 @@ public class Packet02Data extends UDPPakcet {
 
     @Override
     public byte[] getData() {
-        return state.serialize();
+    	byte[]temp = state.serialize();
+    	byte[]newData =new byte[2+temp.length];
+    	newData[0] = '0';
+    	newData[1] = '2';
+    	int count = 2;
+
+    	for(byte b:temp){
+    		newData[count] = b;
+    		count++;
+    	}
+        return newData;
 
     }
 
