a1c58bda80ddcbbad6761f57578b2447faf5dbe9
Claire Sun <claire.szh@gmail.com>
2014-10-07 16:23:18 +1300

    Correct solutions for color collected  items of player

--- src/window/components/CollectItemsCanvas.java
+++ src/window/components/CollectItemsCanvas.java
@@ -20,29 +20,30 @@ public class CollectItemsCanvas extends Canvas {
 	public CollectItemsCanvas(Player player) {
 		this.player = player;
 		type = player.getType();
-		collectItems = new ArrayList<ImageIcon>();
 		this.setBackground(Color.LIGHT_GRAY);
 		this.setBounds(0, 600, 750, 70);
 		//this.setSize(new Dimension(400, 70));
+		initialiseItems();
 		getCollectItems();
 	}
 	
+	private void initialiseItems() {
+		collectItems = new ArrayList<ImageIcon>();
+		for(int i = 0; i < player.getTokenList().size(); i++){
+			String resource = "Resource/" + type.toString() + "/" + type.toString() + " grey" + ".png";
+			collectItems.add(i, new ImageIcon(resource));			
+		}
+	}
+
 	private void getCollectItems() {		
 		for (int i = 0; i < player.getTokenList().size(); i++){
-			if (collectItems.get(i) == null && player.getTokenList().get(i).isFound()){
+			if (player.getTokenList().get(i).isFound()){
 				String color = identifyColor(player.getTokenList().get(i).getColor());
 				String resource = "Resource/" + type.toString() + "/" + type.toString() + " " + color + ".png";
 				collectItems.add(i, new ImageIcon(resource));
 			}
 		}
-		
-		for (int j = 0 ; j < 5; j++){
-			if (collectItems.get(j) == null){
-				String resource = "Resource/" + type.toString() + "/" + type.toString() + " grey" + ".png";
-				collectItems.add(j, new ImageIcon(resource));			
-			}
-		}
-		
+
 		//collectItems.add(new ImageIcon("Resource/diamond/diamond grey.png"));
 
 	}
