406baa6a5dfd1c76278281e7eebe1d46f936c60d
Vivian Stewart <vivichrist@gmail.com>
2014-10-11 15:21:47 +1300

    - new Background image - new control Behaviour forr player

--- src/ui/components/Behaviour.java
+++ src/ui/components/Behaviour.java
@@ -4,7 +4,7 @@ import java.awt.geom.Point2D;
 
 import javax.media.opengl.GL2;
 
-public interface Behavior
+public interface Behaviour
 {
 	public void modify( GL2 gl, Point2D.Float pos );
 	public boolean activate();
--- src/ui/components/Rings.java
+++ src/ui/components/Rings.java
@@ -5,12 +5,17 @@ import java.awt.geom.Point2D.Float;
 
 import javax.media.opengl.GL2;
 
-public class Rings implements Behavior
+public class Rings implements Behaviour
 {
 
 	private static final float ground = GameView.cellsize / 2f
 			, ceiling = GameView.cellsize + GameView.cellsize - ground;
 	private float	move = ground, moveSpeed = 0.1f;
+
+	public Rings( boolean start )
+	{
+		move = start ? ground : ceiling + 1f;
+	}
 
 	@Override
 	public void modify( GL2 gl, Point2D.Float pos )
