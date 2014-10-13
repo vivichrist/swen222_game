87be22c2bb1121142e75c03f4fa4bd6f37f5b665
Claire Sun <claire.szh@gmail.com>
2014-10-14 10:50:13 +1300

    Add temporary GUI test cases

--- src/tests/window/components/GUITests.java
+++ src/tests/window/components/GUITests.java
@@ -1,9 +1,25 @@
 package tests.window.components;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import window.components.GUI;
+
+
 public class GUITests {
 	
+	@Test
+	public void TestName(){
+		GUI gui = new GUI();
+		assertEquals(gui.strServerName, gui.strServerNameC);
+	}
 	
-	
+	@Test
+	public void TestName1(){
+		GUI gui = new GUI();
+		assertEquals(gui.strPortNum, gui.strPortNumC);
+	}
 	
 
 }
